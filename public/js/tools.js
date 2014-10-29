e5f68fc86bb2f198b81d8b70898a1524d75ccadd
cloudbian <cloudbian@gmail.com>
2014-10-29 13:46:03 +0800

    update ajax func

--- public/js/tools.js
+++ public/js/tools.js
@@ -18,7 +18,7 @@ function getEjsTemplate(url,params,containerId){
         type: "get",
         url: url,
         cache:false,
-        async:false,
+//        async:false,
         data:params
     }).done(function(data, textStatus){
             if(data.error&&data.error==2){
